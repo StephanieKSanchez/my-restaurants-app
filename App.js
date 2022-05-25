ac3ee32130cf95b5b15f543b68d75c46bbf0969c
Stephanie Sanchez <stephaniesanchez@Stephanies-MacBook-Air-2.local>
2022-05-25 16:27:02 -0400

    added scrollview & restaurant pics from api & button

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, ActivityIndicator, ImageBackground, ScrollView, Image, Button } from 'react-native'
+
+const image = { uri: 'https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg' }
 
 export default function App() {
   const [allRestaurants, setAllRestaurants] = useState()
 
-  useEffect(async () => {
+  useEffect(() => {
     const getData = async () => {
       try {
         const response = await fetch('https://my-first-firestore-sks.web.app/restaurants/')
@@ -15,26 +17,47 @@ export default function App() {
         console.error(err)
       }
     }
+
     getData()
   }, [])
-  
 
   return (
     <View style={styles.container}>
-      {allRestaurants?.map((singleRestaurant) => (
-        <Text key={singleRestaurant.id} >{singleRestaurant.name} </Text>
-      ))}
-      <Text>What's Good?!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground resizeMode='cover' source={image} style={styles.container}>
+        <ScrollView>
+          {allRestaurants ? (
+            allRestaurants?.map(singleRest => (
+              <>
+                <Text style={styles.restaurantsName} key={singleRest.id}>
+                  {singleRest.name} / {singleRest.cuisine}
+                </Text>
+                <Image source={{ uri: singleRest.image }} style={{ width: '100%', height: 100 }} />
+              </>
+            ))
+          ) : (
+            <ActivityIndicator size='large' color='orange' />
+          )}
+        </ScrollView>
+        <Button title="Press me"
+        color="#f194ff" />
+        <Text style={{ color: 'white', marginBottom: 20 }}>What's Good?!</Text>
+        <StatusBar style='auto' />
+      </ImageBackground>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  restaurantsName: {
+    color: 'white',
+    fontSize: 70,
+    marginVertical: 150,
+  },
+})
