abc0a8ec4aab4bff749975ddf6856ab510e1b9b5
Stephanie Sanchez <stephaniesanchez@Stephanies-MacBook-Air-2.local>
2022-05-25 15:19:05 -0400

    set up ios, browser, desktop app, & connected to api & rendered data to app

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [allRestaurants, setAllRestaurants] = useState()
+
+  useEffect(async () => {
+    const getData = async () => {
+      try {
+        const response = await fetch('https://my-first-firestore-sks.web.app/restaurants/')
+        const data = await response.json()
+        setAllRestaurants(data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    getData()
+  }, [])
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {allRestaurants?.map((singleRestaurant) => (
+        <Text key={singleRestaurant.id} >{singleRestaurant.name} </Text>
+      ))}
+      <Text>What's Good?!</Text>
       <StatusBar style="auto" />
     </View>
   );
