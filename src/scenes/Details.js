34018fb21ba638273b689a8c75107df084da666a
Stephanie Sanchez <stephk006@gmail.com>
2022-05-26 14:35:36 -0400

    used context in details file

--- src/scenes/Details.js
+++ src/scenes/Details.js
@@ -1,10 +1,29 @@
+import { useContext } from 'react';
 import { View, Text, Image } from 'react-native';
+import { ActivityIndicator } from 'react-native';
+import { SingleRestaurantContext } from '../../App';
 import styles from '../styles';
 
 export default function Details() {
+    const { currentRestaurant } = useContext(SingleRestaurantContext)
     return (
         <View style={styles.restaurantCard}>
-            <Text style={styles.restaurantsName} >Details Page</Text>
+            {!currentRestaurant
+            ? <ActivityIndicator />
+            : (
+                <>
+                <Image 
+                source={{ uri: currentRestaurant.image}} 
+                style={{ width: '100%', height: 240 }} />
+                <Text style={styles.restaurantsName} >{currentRestaurant.name}</Text>
+                <Text style={styles.cuisine} >{currentRestaurant.cuisine}</Text>
+                <Text style={styles.address} >{currentRestaurant.address}</Text>
+                <Text style={[styles.address, {fontWeight: '700'}]} >
+                    Rating: {currentRestaurant.rating}
+                </Text>
+                </>
+            )
+        }
         </View>
     )
 }
--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { ScrollView, ActivityIndicator, TouchableOpacity } from "react-native";
+import { SingleRestaurantContext } from "../../App";
 import RestaurantCard from "../components/RestaurantCard";
 
 export default function Home({ navigation }) {
     const [allRestaurants, setAllRestaurants] = useState();
+    const { setCurrentRestaurant } = useContext(SingleRestaurantContext);
 
     useEffect(() =>{
         fetch('https://my-first-firestore-sks.web.app/restaurants/')
@@ -12,13 +14,18 @@ export default function Home({ navigation }) {
         .catch(console.error)
     }, [])
 
+    const handlePress = (singleRestaurant) => {
+        setCurrentRestaurant(singleRestaurant);
+        navigation.navigate('Details')
+    }
+
     return (
         <ScrollView>
             {!allRestaurants
                 ? <ActivityIndicator size='large' color='orange' />
                 : allRestaurants.map(singleRestaurant => (
                     <TouchableOpacity key={singleRestaurant.id} 
-                        onPress={() => navigation.navigate('Details')}>
+                        onPress={() => handlePress(singleRestaurant)}>
                         <RestaurantCard   singleRestaurant={singleRestaurant} />
                     </TouchableOpacity>
                     
