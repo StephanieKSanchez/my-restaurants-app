4e83895d771a6c263cc7b39a0bc1e26d423362c2
Stephanie Sanchez <stephk006@gmail.com>
2022-05-26 14:07:27 -0400

    created context

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-
+import { createContext, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/scenes/Home';
 import Details from './src/scenes/Details';
-const Stack = createNativeStackNavigator()
+
+const Stack = createNativeStackNavigator();
+
+export const SingleRestaurantContext = createContext(null);
 
 export default function App() {
+  const [currentRestaurant, setCurrentRestaurant] = useState();
   return (
-    <NavigationContainer>
+    <SingleRestaurantContext.Provider value={{ currentRestaurant, setCurrentRestaurant }} >
+      <NavigationContainer>
       <Stack.Navigator initialRouteName='' >
         <Stack.Screen name="Home" component={Home} options={{title: 'Local Restaurants'}} />
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
       <StatusBar style='auto' />
     </NavigationContainer>
-    
+    </SingleRestaurantContext.Provider>
   );
 }
